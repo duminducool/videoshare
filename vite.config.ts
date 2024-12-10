7bc90e9ce43650f3269f907842f3e8a043c746f6
duminducool <142707558+duminducool@users.noreply.github.com>
2024-12-10 13:21:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/videoshare/', // Set this to your repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
